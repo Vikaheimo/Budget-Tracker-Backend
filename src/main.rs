eb8c360a2b2e85eb790064f83ec3b500d1b75167
Vili Ikäheimo <vili.vikaheim@gmail.com>
2023-02-26 10:49:04 +0200

    Add Cors

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,15 @@
 
 mod api;
 mod catcher;
+mod middleware;
 
+use middleware::cors;
 use api::version::version;
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
+    .attach(cors::CORS)
     .mount("/api/version", routes![version])
     .register("/",catchers![catcher::not_found])
 }
